320ca1b46877fa1c4ffadf788eace0b831b00839
masa08 <ushimasarug08@gmail.com>
2021-11-14 14:51:27 +0900

    :recycle: create targetDomList class

--- src/index.js
+++ src/index.js
@@ -1,47 +1,19 @@
+import { TargetDomList } from './targetList';
+
 const searchResults = document.getElementsByClassName('yuRUbf');
-// TODO: classで管理する
-let nextTargetIndex = 0;
-let prevTargetIndex = searchResults.length;
+const targetDomList = new TargetDomList(searchResults);
 
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Tab' && e.shiftKey) {
     e.preventDefault();
 
-    if (prevTargetIndex === -1) {
-      prevTargetIndex = searchResults.length - 1;
-      nextTargetIndex = 0;
-    }
-
-    // focus処理
-    const newTarget = getTargetElement(searchResults, prevTargetIndex);
-    focusTargetHTMLElement(newTarget);
-
-    // prevIndexを減算
-    prevTargetIndex = prevTargetIndex - 1;
-    nextTargetIndex = prevTargetIndex + 2;
+    targetDomList.prev();
     return;
   }
   if (e.key === 'Tab') {
     e.preventDefault();
 
-    if (nextTargetIndex === searchResults.length) nextTargetIndex = 0;
-
-    // focus処理
-    const newTarget = getTargetElement(searchResults, nextTargetIndex);
-    focusTargetHTMLElement(newTarget);
-
-    // indexを加算
-    nextTargetIndex = nextTargetIndex + 1;
-    prevTargetIndex = nextTargetIndex - 2;
+    targetDomList.next();
     return;
   }
 });
-
-const getTargetElement = (arr, index) => {
-  return arr[index].getElementsByTagName('a')[0];
-};
-
-const focusTargetHTMLElement = (target) => {
-  target.setAttribute('class', 'focus-visible');
-  target.focus();
-};
