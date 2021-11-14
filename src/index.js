ff6df4c13f1b4e1d5001df18dd69bbd39ba73895
masa08 <ushimasarug08@gmail.com>
2021-11-14 14:06:14 +0900

    :sparkles: add shift tab back

--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
 const searchResults = document.getElementsByClassName('yuRUbf');
-let index = 0;
+let nextTargetIndex = 0;
+let prevTargetIndex = searchResults.length;
 
 document.addEventListener('keydown', (e) => {
-  if (e.key === 'Tab') {
+  if (e.key === 'Tab' && e.shiftKey) {
     e.preventDefault();
-    // 前回focus解除
-    // const prevTarget = searchResults[index].getElementsByTagName('a')[0];
-    // prevTarget.removeAttribute('class', 'focus-visible');
 
-    if (index === searchResults.length) index = 0;
+    if (prevTargetIndex === -1) {
+      prevTargetIndex = searchResults.length - 1;
+      nextTargetIndex = 0;
+    }
 
     // focus処理
-    const newTarget = searchResults[index].getElementsByTagName('a')[0];
-    newTarget.setAttribute('class', 'focus-visible');
-    newTarget.focus();
+    const newTarget = getTargetElement(searchResults, prevTargetIndex);
+    focusTargetHTMLElement(newTarget);
+
+    // prevIndexを減算
+    prevTargetIndex = prevTargetIndex - 1;
+    nextTargetIndex = prevTargetIndex + 2;
+    return;
+  }
+  if (e.key === 'Tab') {
+    e.preventDefault();
+
+    if (nextTargetIndex === searchResults.length) nextTargetIndex = 0;
+
+    // focus処理
+    const newTarget = getTargetElement(searchResults, nextTargetIndex);
+    focusTargetHTMLElement(newTarget);
 
     // indexを加算
-    index = index + 1;
+    nextTargetIndex = nextTargetIndex + 1;
+    prevTargetIndex = nextTargetIndex - 2;
+    return;
   }
 });
+
+const getTargetElement = (arr, index) => {
+  return arr[index].getElementsByTagName('a')[0];
+};
+
+const focusTargetHTMLElement = (target) => {
+  target.setAttribute('class', 'focus-visible');
+  target.focus();
+};
