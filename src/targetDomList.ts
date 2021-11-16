7da48f9deb1c2936407a304e8bf6653851a45a23
masa08 <ushimasarug08@gmail.com>
2021-11-16 21:41:16 +0900

    :recycle: refactor

--- src/targetDomList.ts
+++ src/targetDomList.ts
@@ -1,48 +1,62 @@
 export class TargetDomList {
   list: HTMLAnchorElement[];
   prevIndex: number;
+  currentIndex: number | undefined;
   nextIndex: number;
 
   constructor(list: HTMLAnchorElement[]) {
     this.list = list;
     this.prevIndex = this.list.length - 1;
+    this.currentIndex = undefined;
     this.nextIndex = 0;
-  }
-
-  prev(): void {
-    const newTargetDom = this.list[this.prevIndex];
-    this.#addFocus(newTargetDom);
-    this.#movePrev();
   }
 
   next(): void {
     const newTargetDom = this.list[this.nextIndex];
     this.#addFocus(newTargetDom);
+    this.currentIndex = this.nextIndex;
     this.#moveNext();
   }
 
+  prev(): void {
+    const newTargetDom = this.list[this.prevIndex];
+    this.#addFocus(newTargetDom);
+    this.currentIndex = this.prevIndex;
+    this.#movePrev();
+  }
+
   #moveNext(): void {
-    if (this.nextIndex >= this.list.length - 1) {
-      this.#resetIndex();
-      return;
-    }
+    if (this.#isOutOfRange()) return;
     this.nextIndex = this.nextIndex + 1;
-    this.prevIndex =
-      this.nextIndex - 2 === -1 ? this.list.length - 1 : this.nextIndex - 2;
+    this.prevIndex = this.nextIndex - 2;
   }
 
   #movePrev(): void {
-    if (this.prevIndex <= 0) {
-      this.#resetIndex();
-      return;
-    }
+    if (this.#isOutOfRange()) return;
     this.prevIndex = this.prevIndex - 1;
-    this.nextIndex =
-      this.prevIndex + 2 === this.list.length ? 0 : this.prevIndex + 2;
+    this.nextIndex = this.prevIndex + 2;
   }
 
-  #resetIndex(): void {
+  #isOutOfRange(): boolean {
+    if (this.currentIndex === 0) {
+      this.#setIndexForfirstFocus();
+      return true;
+    }
+    if (this.currentIndex === this.list.length - 1) {
+      this.#setIndexForLastFocus();
+      return true;
+    }
+
+    return false;
+  }
+
+  #setIndexForfirstFocus(): void {
     this.prevIndex = this.list.length - 1;
+    this.nextIndex = 1;
+  }
+
+  #setIndexForLastFocus(): void {
+    this.prevIndex = this.list.length - 2;
     this.nextIndex = 0;
   }
 
